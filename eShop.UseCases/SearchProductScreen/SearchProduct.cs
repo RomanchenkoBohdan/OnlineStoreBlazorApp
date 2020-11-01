d5809ca645aa9f3538fd55af1e0a6c38564079f1
Bohdan Romanchenko <rov4ak3@gmail.com>
2020-11-01 13:33:51 +0200

    Add project files.

---
+++ eShop.UseCases/SearchProductScreen/SearchProduct.cs
@@ -0,0 +1,20 @@
+﻿using eShop.CoreBusiness.Models;
+using eShop.UseCases.PluginInterfaces.DataStore;
+using System.Collections.Generic;
+
+namespace eShop.UseCases.SearchProductScreen
+{
+    public class SearchProduct : ISearchProduct
+    {
+        private readonly IProductRepositery productRepositery;
+
+        public SearchProduct(IProductRepositery productRepositery)
+        {
+            this.productRepositery = productRepositery;
+        }
+        public IEnumerable<Product> Execute(string filter = null)
+        {
+            return productRepositery.GetProducts(filter);
+        }
+    }
+}
---
+++ eShop.UseCases/SearchProductScreen/ViewProduct.cs
@@ -0,0 +1,19 @@
+﻿using eShop.CoreBusiness.Models;
+using eShop.UseCases.PluginInterfaces.DataStore;
+
+namespace eShop.UseCases.SearchProductScreen
+{
+    public class ViewProduct : IViewProduct
+    {
+        private readonly IProductRepositery productRepositery;
+
+        public ViewProduct(IProductRepositery productRepositery)
+        {
+            this.productRepositery = productRepositery;
+        }
+        public Product Execute(int id)
+        {
+            return productRepositery.GetProduct(id);
+        }
+    }
+}
